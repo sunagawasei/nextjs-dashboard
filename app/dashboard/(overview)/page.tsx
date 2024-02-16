a44373e0b00eb689192a7d70a1f7b913b6ac7544
sunagawasei <sunagawa_sei@ca-adv.co.jp>
2024-02-16 17:05:10 +0900

    UIコンポーネントを更新しました

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,16 +1,15 @@
 import { lusitana } from '@/app/ui/fonts';
 import {
-  fetchCardData,
-  fetchLatestInvoices,
-  fetchRevenue,
-} from '../../lib/data';
+  LatestInvoicesSkeleton,
+  RevenueChartSkeleton,
+} from '@/app/ui/skeletons';
+import { Suspense } from 'react';
+import { fetchCardData } from '../../lib/data';
 import { Card } from '../../ui/dashboard/cards';
 import LatestInvoices from '../../ui/dashboard/latest-invoices';
 import RevenueChart from '../../ui/dashboard/revenue-chart';
 
 export default async function Page() {
-  const revenue = await fetchRevenue();
-  const latestInvoices = await fetchLatestInvoices();
   const {
     totalPaidInvoices,
     totalPendingInvoices,
@@ -33,8 +32,12 @@ export default async function Page() {
         />
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <RevenueChart revenue={revenue} />
-        <LatestInvoices latestInvoices={latestInvoices} />
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
       </div>
     </main>
   );
